c7c3c37c9e6061fac5a5f43289572de13012556b
ornis1 <fastmarat23@gmail.com>
2020-10-03 22:39:43 +0300

    Added avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-avatar v-bind="$attrs" :color="$attrs.color" class="mt-auto">
+    <span class="white--text headline text-uppercase"> {{ name[0] }}</span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style></style>
